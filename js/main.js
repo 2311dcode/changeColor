cf72e8e8ceedc3b2537d638626cdabdca14c9892
phoebe7010 <phoebe7010@daum.net>
2023-11-13 23:18:17 +0900

    로딩시 박스색 첫번째 버튼색으로 변경

--- js/main.js
+++ js/main.js
@@ -1,19 +1,33 @@
 const wrap = document.querySelector('section');
 const btns = wrap.querySelectorAll("li");
 
+//로딩시 박스색을 첫번째 버튼의 텍스트로 변경 
+wrap.style.backgroundColor = btns[0].innerText;
+
+//버튼의 갯수대로 반복
 btns.forEach((btn, idx) => {
+  //버튼 클릭시 
   btn.addEventListener('click', (e) => {
+    //클릭한 버튼에 on클랙스가 있다면 아래 코드를 실행하지 않고 중지 
     if (e.currentTarget.classList.contains('on')) return;
-    activation(btns, idx);
-    changeBg(btns, idx);
+    //함수 호출 
+    activation(btns, e);
+    changeBg(e, wrap);
   });
 });
 
-function activation(arr, idx) {
+//버튼 활성화 함수 정의  
+function activation(arr, e) {
+  //배열 반복을 돌며 요소에 on클래스 제거 
   arr.forEach(el => el.classList.remove('on'));
-  arr[idx].classList.add('on');
+  //클릭한 버튼에 on클래스 추가 
+  e.currentTarget.classList.add('on');
 }
-function changeBg(arr, idx) {
-  let bgColor = arr[idx].innerText;
-  wrap.style.backgroundColor = bgColor;
+
+//박스배경색 변경 함수 정의 
+function changeBg(e, box) {
+  //클릭한 버튼의 텍스트를 변수로 지정 
+  let bgColor = e.currentTarget.innerText;
+  //박스의 배경색에 변수 대입
+  box.style.backgroundColor = bgColor;
 }
